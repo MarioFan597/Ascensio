81d21ba560c51416bdc9fbaf0b38835dcf01ef1e
MarioFan597 <81440403+MarioFan597@users.noreply.github.com>
2025-06-26 19:44:31 -0400

    Typo fix

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -396,7 +396,7 @@ return {
 				name = "Apotheosis Canadite",
 				text = {
 				"This Joker can reach",
-				"a {C:entr_entropic,E:1}Even Higher Form{}"
+				"an {C:entr_entropic,E:1}Even Higher Form{}"
 				}
 			}
 		},
