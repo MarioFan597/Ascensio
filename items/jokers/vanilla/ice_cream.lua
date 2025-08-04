bb11c69d2632dfc24fc73b8ad11d9c81a4c53cbe
Rh4096 <rhelvetican@gmail.com>
2025-08-04 15:09:16 +0700

    Slight tweaking

--- items/jokers/vanilla/ice_cream.lua
+++ items/jokers/vanilla/ice_cream.lua
@@ -8,18 +8,25 @@ SMODS.Joker({
 	soul_pos = { x = 8, y = 9, extra = { x = 7, y = 9 } },
 	cost = 50,
 
-	config = { extra = { chips = to_big(1) } },
+	config = { extra = { chips = to_big(1), flag = false } },
 
 	loc_vars = function(_, _, card)
-		return {
-			vars = {
-				card.ability.extra.chips,
-			},
-		}
+		if card.ability.extra.flag then
+			return {
+				vars = {
+					card.ability.extra.chips,
+				},
+			}
+		else
+			return {
+				vars = { 1 },
+			}
+		end
 	end,
 
 	calculate = function(_, card, context)
 		if context.before then
+			card.ability.extra.flag = true
 			card.ability.extra.chips = card.ability.extra.chips * 2
 
 			card_eval_status_text(
