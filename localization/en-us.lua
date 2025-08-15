1ddc68112103d4d45d3b1ad6320d846fce03b252
MarioFan597 <81440403+MarioFan597@users.noreply.github.com>
2025-08-15 12:53:28 -0400

    Thantos Text Rearrangment

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -634,9 +634,15 @@ return {
 			j_asc_canio = {
 				name = "Thanatos",
 				text = {
-					"Gains {X:dark_edition,C:white}^#2#{} Mult per destroyed {C:attention}face{} card,",
-					"destroyed {C:attention}face{} cards are returned to hand",
+					{
+					"Gains {X:dark_edition,C:white}^#2#{} Mult per",
+					"destroyed {C:attention}face{} card",
 					"{C:inactive}(Currently {X:dark_edition,C:white}^#1#{}{C:inactive} Mult)",
+					},
+					{
+					"Destroyed {C:attention}face{} cards",
+					"are returned to hand",
+					},	
 				},
 			},
 
