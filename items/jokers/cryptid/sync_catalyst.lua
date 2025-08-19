08b83322ece201eab928ccd95e54c2102670b992
Rh4096 <rhelvetican@gmail.com>
2025-08-19 18:22:24 +0700

    clarification4

--- items/jokers/cryptid/sync_catalyst.lua
+++ items/jokers/cryptid/sync_catalyst.lua
@@ -58,11 +58,58 @@ SMODS.Joker({
 		end
 
 		if context.final_scoring_step then
+			---@diagnostic disable-next-line: undefined-global
+			local val = (hand_chips + mult) / 2
+			---@diagnostic disable-next-line: undefined-global
+			mult = mod_mult(val)
+			---@diagnostic disable-next-line: undefined-global
+			hand_chips = mod_chips(total / 2)
+
+			---@diagnostic disable-next-line: undefined-global
+			update_hand_text({ delay = 0 }, { chips = hand_chips, mult = mult })
+
+			G.E_MANAGER:add_event(Event({
+				func = function()
+					-- scored_card:juice_up()
+					play_sound("gong", 0.94, 0.3)
+					play_sound("gong", 0.94 * 1.5, 0.2)
+					play_sound("tarot1", 1.5)
+					ease_colour(G.C.UI_CHIPS, { 0.8, 0.45, 0.85, 1 })
+					ease_colour(G.C.UI_MULT, { 0.8, 0.45, 0.85, 1 })
+					G.E_MANAGER:add_event(Event({
+						trigger = "after",
+						blockable = false,
+						blocking = false,
+						delay = 0.8,
+						func = function()
+							ease_colour(G.C.UI_CHIPS, G.C.BLUE, 0.8)
+							ease_colour(G.C.UI_MULT, G.C.RED, 0.8)
+							return true
+						end,
+					}))
+					G.E_MANAGER:add_event(Event({
+						trigger = "after",
+						blockable = false,
+						blocking = false,
+						no_delete = true,
+						delay = 1.3,
+						func = function()
+							G.C.UI_CHIPS[1], G.C.UI_CHIPS[2], G.C.UI_CHIPS[3], G.C.UI_CHIPS[4] =
+								G.C.BLUE[1], G.C.BLUE[2], G.C.BLUE[3], G.C.BLUE[4]
+							G.C.UI_MULT[1], G.C.UI_MULT[2], G.C.UI_MULT[3], G.C.UI_MULT[4] =
+								G.C.RED[1], G.C.RED[2], G.C.RED[3], G.C.RED[4]
+							return true
+						end,
+					}))
+					return true
+				end,
+			}))
+
+			delay(0.6)
+
 			return {
-				balance = true,
 				message = "Balanced!",
 				colour = G.C.DARK_EDITION,
-				func = balance_sound,
 			}
 		end
 
