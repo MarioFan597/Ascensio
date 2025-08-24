35ceb50411198e55c8780b4fb2a8d8ab44ae04e3
Rh4096 <rhelvetican@gmail.com>
2025-08-24 10:56:30 +0700

    more number api

--- lib/number.lua
+++ lib/number.lua
@@ -2,13 +2,20 @@
 ---@alias fn<T> fun(...): T
 
 -- Cache the function for performance and stuffs.
----@type fn<any>, fn<number>
-local to_big, to_number = to_big, to_number
+---@type fn<any>, fn<number>, fn<any>
+local to_big, to_number, lenient_bignum = to_big, to_number, lenient_bignum
 
 -- Ensure the Number table exists.
 if not Number then
 	Number = {}
 end
+
+---Positive Infinity value.
+---@type number
+Number.Inf = 1 / 0
+---Negative Infinity value.
+---@type number
+Number.NegInf = -Number.Inf
 
 ---Return the largest integer that is smaller than the given number.
 ---@param n TalismanNumber
@@ -78,6 +85,13 @@ function Number.toNumber(n)
 	return to_number(n)
 end
 
+---Convert the given number into a number.
+---@param n TalismanNumber
+---@return number
+function Number.lenientBignum(n)
+	return lenient_bignum(n)
+end
+
 ---Exponentiate two given numbers.
 ---@param a TalismanNumber
 ---@param b TalismanNumber
@@ -101,4 +115,39 @@ end
 ---@return TalismanNumber
 function Number.hyper(a, b, arrows)
 	return Number.toBig(a):arrow(arrows, b)
+end
+
+---Checks if the given number is NaN.
+---@param n TalismanNumber
+---@return boolean
+function Number.isNaN(n)
+	if type(n) == "number" then
+		return n ~= n
+	end
+
+	return n:isNaN()
+end
+
+---Returns the smaller value of two supplied numbers.
+---@param a TalismanNumber
+---@param b TalismanNumber
+---@return TalismanNumber
+function Number.min(a, b)
+	if type(a) == "number" and type(b) == "number" then
+		return math.min(a, b)
+	end
+
+	return Number.toBig(a):min(b)
+end
+
+---Returns the bigger value of two supplied numbers.
+---@param a TalismanNumber
+---@param b TalismanNumber
+---@return TalismanNumber
+function Number.max(a, b)
+	if type(a) == "number" and type(b) == "number" then
+		return math.max(a, b)
+	end
+
+	return Number.toBig(a):max(b)
 end
