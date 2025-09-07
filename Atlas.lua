957222ffe5ecf0d21020709fcfa18031efee9e34
Rh4096 <rhelvetican@gmail.com>
2025-09-07 11:17:09 +0700

    fmt

--- Atlas.lua
+++ Atlas.lua
@@ -1,66 +1,66 @@
 if CardSleeves then
-	SMODS.Atlas({
-		key = "sleeves",
-		path = "sleeves.png",
-		px = 73,
-		py = 95,
-	})
+    SMODS.Atlas({
+        key = "sleeves",
+        path = "sleeves.png",
+        px = 73,
+        py = 95,
+    })
 end
 
 if Entropy then
-	SMODS.Atlas({
-		key = "v_entr_atlas_1",
-		path = "jokers/vanilla_entropics/vanilla_entropic_atlas_1.png",
-		px = 71,
-		py = 95,
-	})
+    SMODS.Atlas({
+        key = "v_entr_atlas_1",
+        path = "jokers/vanilla_entropics/vanilla_entropic_atlas_1.png",
+        px = 71,
+        py = 95,
+    })
 
-	SMODS.Atlas({
-		key = "e_atlas_1",
-		path = "jokers/entropy/entropy_atlas_1.png",
-		px = 71,
-		py = 95,
-	})
+    SMODS.Atlas({
+        key = "e_atlas_1",
+        path = "jokers/entropy/entropy_atlas_1.png",
+        px = 71,
+        py = 95,
+    })
 end
 
 SMODS.Atlas({
-	key = "modicon",
-	path = "modicon.png",
-	px = 34,
-	py = 34,
+    key = "modicon",
+    path = "modicon.png",
+    px = 34,
+    py = 34,
 })
 
 SMODS.Atlas({
-	key = "v_atlas_1",
-	path = "jokers/vanilla/vanilla_atlas_1.png",
-	px = 71,
-	py = 95,
+    key = "v_atlas_1",
+    path = "jokers/vanilla/vanilla_atlas_1.png",
+    px = 71,
+    py = 95,
 })
 
 SMODS.Atlas({
-	key = "c_atlas_1",
-	path = "jokers/cryptid/cryptid_atlas_1.png",
-	px = 71,
-	py = 95,
+    key = "c_atlas_1",
+    path = "jokers/cryptid/cryptid_atlas_1.png",
+    px = 71,
+    py = 95,
 })
 
 SMODS.Atlas({
-	key = "c_atlas_mortal",
-	path = "jokers/cryptid/cryptid_mortals_atlas.png",
-	px = 71,
-	py = 95,
+    key = "c_atlas_mortal",
+    path = "jokers/cryptid/cryptid_mortals_atlas.png",
+    px = 71,
+    py = 95,
 })
 
 SMODS.Atlas({
-	key = "sticker_atlas",
-	path = "ascensio_sticker_atlas.png",
-	px = 71,
-	py = 95,
+    key = "sticker_atlas",
+    path = "ascensio_sticker_atlas.png",
+    px = 71,
+    py = 95,
 })
 
 SMODS.Atlas({
-	key = "deck_atlas",
-	path = "deck_atlas.png",
-	px = 71,
-	py = 95,
+    key = "deck_atlas",
+    path = "deck_atlas.png",
+    px = 71,
+    py = 95,
 })
