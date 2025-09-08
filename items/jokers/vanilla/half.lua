383e753ee8f50c95e3389614d8681b42c6166e37
Rh4096 <rhelvetican@gmail.com>
2025-09-08 10:47:40 +0700

    doing shit

--- items/jokers/vanilla/half.lua
+++ items/jokers/vanilla/half.lua
@@ -1,6 +1,5 @@
 SMODS.Joker({
     key = "half",
-    config = { extra = { mult = 1.2, max_play = 2 } },
     rarity = "cry_exotic",
     atlas = "v_atlas_1",
     blueprint_compat = true,
@@ -9,15 +8,19 @@ SMODS.Joker({
     soul_pos = { x = 5, y = 14, extra = { x = 4, y = 14 } },
     cost = 50,
     order = 16,
-    loc_vars = function(self, info_queue, card)
+
+    config = { extra = { mult = 1.2, max_play = 2 } },
+
+    loc_vars = function(_, _, card)
         return {
             vars = {
-                card and lenient_bignum(card.ability.extra.mult),
-                card and lenient_bignum(card.ability.extra.max_play),
+                card.ability.extra.mult,
+                card.ability.extra.max_play,
             },
         }
     end,
-    calculate = function(self, card, context) --I LOVE LUASLOP!!!! LUASLOP MY BELOVED!!!
+
+    calculate = function(_, card, context) --I LOVE LUASLOP!!!! LUASLOP MY BELOVED!!!
         if context.joker_main or context.forcetrigger then
             if #context.full_hand <= card.ability.extra.max_play or context.forcetrigger then
                 return {
@@ -26,8 +29,6 @@ SMODS.Joker({
                     colour = G.C.DARK_EDITION,
                     card = card,
                 }
-            else
-                return nil
             end
         end
     end,
